a807485f9de29d511e158525308e7b4da2a24b37
JakariaPUST <jakaria.pust@gmail.com>
2021-07-05 19:18:16 +0600

    add to cart and quantity selection done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header'
 
 import HomeScreens from './sreens/HomeScreens'
 import ProductScreen from './sreens/ProductScreen'
+import CartScreen from './sreens/CartScreen'
 function App() {
   return (
     <Router>
@@ -15,6 +16,7 @@ function App() {
         <Container>
         <Route path='/' component={HomeScreens} exact/>
         <Route path='/product/:id' component={ProductScreen} />
+        <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer></Footer>
